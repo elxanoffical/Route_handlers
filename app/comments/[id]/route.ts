b7876b8796d18b7db8d0b12a072dce45dc552aaa
Elxan <elxan.offical.07@gmail.com>
2025-02-05 22:33:28 +0400

    Redirects in Route Handlers

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -1,10 +1,14 @@
-import { comment } from "postcss";
+
+import { redirect } from "next/navigation";
 import { comments } from "../data";
 
 export async function GET(
   _request: Request, // bu numunede request istifade olunmadiqina gore evveline (_) isaret qoyulur
   { params }: { params: { id: string } } // Burada id adında bir parametr gözlənilir və bu parametr string kimi təyin olunub.
 ) {
+  if(parseInt(params.id) > comments.length){   //  Bu hissədə, əgər "params.id" dəyəri comments massivinin uzunluğundan böyükdürsə, istifadəçi /comments səhifəsinə yönləndirilir.
+    redirect("/comments")
+  }
   const comment = comments.find(
     (comment) => comment.id === parseInt(params.id) // massivdəki hər bir şərhin id dəyərini, URL-dən gələn (params.id) dəyəri ilə müqayisə edir.
   );
@@ -20,8 +24,7 @@ export async function PATCH(
 
   const { text } = body; // JSON məlumatından text adlı xüsusiyyəti çıxarır
 
-  const index = comments.findIndex(
-    // "comments" massivində şərti ödəyən ilk elementin indeksini tapır.
+  const index = comments.findIndex(  // "comments" massivində şərti ödəyən ilk elementin indeksini tapır.
     (comment) => comment.id === parseInt(params.id) //  "params.id" string olduğu üçün parseInt ilə ədədə (integer) => "tam eded" çevrilir.
   );
   comments[index].text = text; //   Tapılan şərhə istinad edir.
