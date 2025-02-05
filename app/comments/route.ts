a29dddd0596860fe2898d05ce18aba35acce8763
Elxan <elxan.offical.07@gmail.com>
2025-02-05 22:15:45 +0400

    URL Query parameters

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -1,7 +1,15 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-  return Response.json(comments);
+export async function GET(reuqest: NextRequest) {
+
+   const searchParams = reuqest.nextUrl.searchParams  //  Sorğunun URL-dəki parametrlərini ehtiva edir.
+   const query = searchParams.get("query")  // query parametrinin dəyəri alınır
+   const filteredComments = query
+   ? comments.filter((comment)=> comment.text.includes(query))  //includes(query): query dəyərinin "comment.text" içində olub-olmadığını yoxlayır. Əgər varsa, true qaytarır; yoxdursa, false.
+   : comments
+  return Response.json(filteredComments);
+
 }
 
 export async function POST(request: Request) {
