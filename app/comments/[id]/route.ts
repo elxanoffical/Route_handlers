9601e7962f7f380545ed5d95930820a64f32b2be
Elxan <elxan.offical.07@gmail.com>
2025-02-05 19:35:41 +0400

    PATCH request

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -1,26 +1,28 @@
 import { comments } from "../data";
 
 export async function GET(
-  _request: Request, // bu numunede request istifade olunmadiqina gore evveline (_) isaret qoyulur
-  { params }: { params: { id: string } } // Burada id adında bir parametr gözlənilir və bu parametr string kimi təyin olunub.
+  _request: Request,   // bu numunede request istifade olunmadiqina gore evveline (_) isaret qoyulur
+  { params }: { params: { id: string } }   // Burada id adında bir parametr gözlənilir və bu parametr string kimi təyin olunub.
 ) {
   const comment = comments.find(
-    (comment) => comment.id === parseInt(params.id) // massivdəki hər bir şərhin id dəyərini, URL-dən gələn (params.id) dəyəri ilə müqayisə edir.
+    (comment) => comment.id === parseInt(params.id)   // massivdəki hər bir şərhin id dəyərini, URL-dən gələn (params.id) dəyəri ilə müqayisə edir.
   );
-  return Response.json(comment); // Tapılan şərh (comment), JSON formatında bir HTTP cavabı (response) kimi qaytarılır.
+  return Response.json(comment);   // Tapılan şərh (comment), JSON formatında bir HTTP cavabı (response) kimi qaytarılır.
 }
 
 
-// export async function PATCH(
-//   request: Request,
+export async function PATCH(
+  request: Request,
 
-//   { params }: { params: { id: string } }
-// ) {
-//   const body = await request.json();
-//   const { text } = body;
-//   const index = comments.findIndex(
-//     (comment) => comment.id === parseInt(params.id)
-//   );
-//   comments[index].text = text;
-//   return Response.json(comments[index]);
-// }
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();       //  (body) göndərilən JSON məlumatını oxuyur. Bu məlumat asinxron olduğu üçün await istifadə olunur.
+
+  const { text } = body;       // JSON məlumatından text adlı xüsusiyyəti çıxarır
+  
+  const index = comments.findIndex(    // "comments" massivində şərti ödəyən ilk elementin indeksini tapır.
+    (comment) => comment.id === parseInt(params.id)  //  "params.id" string olduğu üçün parseInt ilə ədədə (integer) => "tam eded" çevrilir.
+  );
+  comments[index].text = text;   //   Tapılan şərhə istinad edir.
+  return Response.json(comments[index]);   //   Şərhin "text" xüsusiyyətini sorğu gövdəsində göndərilən yeni mətn ilə yeniləyir.
+}
