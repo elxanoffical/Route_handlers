5bded618f55c42c3d41e057f3ac3923674668616
Elxan <elxan.offical.07@gmail.com>
2025-02-06 20:50:57 +0400

    .

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -2,7 +2,7 @@ import { type NextRequest } from "next/server";
 import { headers } from "next/headers";
 
 export async function GET(reuqest: NextRequest) {
-  const requestHeaders = new Headers(reuqest.headers);
+  const requestHeaders = new Headers(reuqest.headers);  //  Funksiya, NextRequest növündə bir parametr qəbul edir. Bu parametr, gələn HTTP sorğusunu təmsil edir.
 
   // const headerList = headers()
   console.log(requestHeaders.get("Authorization"));
@@ -10,7 +10,7 @@ export async function GET(reuqest: NextRequest) {
 
   return new Response("<h1>Profile API data</h1>", {
     headers: {
-        "Content-Type": "text/html"
+        "Content-Type": "text/html"  //   Content-Type başlığı text/html olaraq təyin edilir, beləliklə brauzer bu cavabı bir HTML sənədi kimi şərh edir.
     }
   });
 }
