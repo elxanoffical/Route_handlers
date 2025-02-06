7a49408a07d07658f4a59a3a68398d7ca8a52181
Elxan <elxan.offical.07@gmail.com>
2025-02-06 21:23:55 +0400

    set() works problematic

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,16 +1,19 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers,cookies } from "next/headers";
 
-export async function GET(reuqest: NextRequest) {
-  const requestHeaders = new Headers(reuqest.headers);  //  Funksiya, NextRequest növündə bir parametr qəbul edir. Bu parametr, gələn HTTP sorğusunu təmsil edir.
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);  //  Funksiya, NextRequest növündə bir parametr qəbul edir. Bu parametr, gələn HTTP sorğusunu təmsil edir.
 
-  // const headerList = headers()
+  cookies().set("resultsCookie", "20")     // kod isleyir amma set-in alti qizarir
+  const theme =  request.cookies.get("theme")
   console.log(requestHeaders.get("Authorization"));
-  // console.log(headerList.get("Authorization"))
+
+  console.log(theme)
 
   return new Response("<h1>Profile API data</h1>", {
     headers: {
-        "Content-Type": "text/html"  //   Content-Type başlığı text/html olaraq təyin edilir, beləliklə brauzer bu cavabı bir HTML sənədi kimi şərh edir.
+        "Content-Type": "text/html",
+        "Set-cookie": "theme=dark"  //   Content-Type başlığı text/html olaraq təyin edilir, beləliklə brauzer bu cavabı bir HTML sənədi kimi şərh edir.
     }
   });
 }
